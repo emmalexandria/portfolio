749f7c44f7feac8417996dace59082cc4459c214
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-11 19:05:30 +0200

    please function

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-    images: {
-        unoptimized: true,
-      },
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
+
+
+const nextConfig = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: {
+        loader:'imgix',
+        path:'emmalexandria.imgix.net',
+    }
+}
+
 
 module.exports = nextConfig
