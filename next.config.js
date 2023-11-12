ddfa3e1708effb88cb43499274eaea4b9376e5e9
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-12 06:18:13 +0200

    give up on gh-pages

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const nextConfig = {}
 
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-console.log(basePath)
-
-const nextConfig = {
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-    images: {
-        loader:'imgix',
-        path:'emmalexandria.imgix.net',
-    }
-}
 
 
 module.exports = nextConfig
