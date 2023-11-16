4f9d2b64a9bc75c636257fcd1fa13e32f4ec7a63
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-16 14:31:45 +0200

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import ContactButton from './ContactButton'
 import FirstPage from './FirstPage'
-import AnimatedDiamond from './AnimatedDiamond'
-import AboutPage from './AboutPage'
 import ProjectsPage from './ProjectsPage'
-import BlogButton from './BlogButton'
 import ContactFooter from './ContactFooter'
 import AboutMeGrid from './AboutMeGrid'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Emma Alexandria's Portfolio",
+  description: "I'm a software developer from Cape Town. I work in web development, but that was kind of an accident.",
+  creator: "Emma Alexandria",
+  keywords: ["Portfolio", "Full stack developer", "South Africa"]
+}
 
 export default function Home() {
-
-
-
   return (
     <main className="bg-light-900 w-full h-full" >
       <FirstPage />
