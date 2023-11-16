9c4b4cb6c773347c2699434a9f9e0000e45189ed
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-16 11:51:16 +0200

    update personal blog

--- app/ProjectsPage.tsx
+++ app/ProjectsPage.tsx
@@ -7,7 +7,7 @@ export default function ProjectsPage() {
 
                     <h2 className="font-display text-5xl sm:text-7xl">Projects</h2>
                     <ul className="space-y-4 mt-8">
-                        <ProjectListItem n={1} technologies={["SvelteKit", "SCSS"]} url="https://www.github.com/emmalexandria/blog" name="Personal blog" />
+                        <ProjectListItem n={1} technologies={["NextJS, MDX, Contentlayer"]} url="https://www.github.com/emmalexandria/next-blog" name="Personal blog" />
                         <ProjectListItem n={2} technologies={["Tauri", "SvelteKit", "Typescript"]} url="https://www.github.com/emmalexandria/zenwriter" name="ZenWriter" />
                     </ul>
 
